b2b803fe897d49ee4be083ae56c081531da10679
akashbind12 <97519781+akashbind12@users.noreply.github.com>
2022-07-24 22:49:22 +0530

    intial

---
+++ db.js
@@ -0,0 +1,11 @@
+// const new_arrival = require("./data/new_arrival.json")
+// const farniture = require("./data/farniture.json")
+// const lighting = require("./data/lighting.json")
+// const cart = require("./data/cart.json")
+
+// module.exports = {
+//     new_arrival,
+//     farniture,
+//     lighting,
+//     cart 
+// }
